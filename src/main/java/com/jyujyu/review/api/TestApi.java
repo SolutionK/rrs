eb6c9e4e2ef28891203894ea4dcab6199a976350
Kim Jongmin <kimjm0226@naver.com>
2024-03-10 15:24:28 +0900

    first commit

---
+++ src/main/java/com/jyujyu/review/api/TestApi.java
@@ -0,0 +1,26 @@
+package com.jyujyu.review.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class TestApi {
+    @GetMapping("/hello/world")
+    public String helloWorld() {
+        return "[Get] Hello, world!";
+    }
+
+    @PostMapping("/hello/world")
+    public String postHelloWord() {
+        return "[Post] Hello, world";
+    }
+
+    @PutMapping("/hello/world")
+    public String putHelloWorld() {
+        return "[Put] Hello, world";
+    }
+
+    @DeleteMapping("/hello/world")
+    public String deleteHelloWorld() {
+        return "[Delete] Hello, world";
+    }
+}
---
+++ src/main/java/com/jyujyu/review/repository/TestRepository.java
@@ -0,0 +1,11 @@
+package com.jyujyu.review.repository;
+
+import com.jyujyu.review.model.TestEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+public interface TestRepository extends JpaRepository<TestEntity, Long>, TestRepositoryCustom {
+    public List<TestEntity> findAllByName(String name);
+}
