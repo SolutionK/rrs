76f69c58d5f1294c19b59e45e60a5a57d773b237
Kim Jongmin <kimjm0226@naver.com>
2024-03-10 16:39:44 +0900

    맛집 생성/수정/삭제 API 완성

--- src/main/java/com/jyujyu/review/api/RestaurantApi.java
+++ src/main/java/com/jyujyu/review/api/RestaurantApi.java
@@ -1,10 +1,15 @@
 package com.jyujyu.review.api;
 
 import com.jyujyu.review.api.request.CreateAndEditRestaurantRequest;
+import com.jyujyu.review.model.RestaurantEntity;
+import com.jyujyu.review.service.RestaurantService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 public class RestaurantApi {
+    private final RestaurantService restaurantService;
     @GetMapping("/restaurants")
     public String getRestaurants() {
         return "This is getRestaurants";
@@ -18,26 +23,24 @@ public class RestaurantApi {
     }
 
     @PostMapping("/restaurant")
-    public String crateRestaurant(
+    public void crateRestaurant(
             @RequestBody CreateAndEditRestaurantRequest request
             ) {
-        return "This is createRestaurant, name=" + request.getName() + ", address=" + request.getAddress()
-                + ", menu[0].name=" + request.getMenus().get(0).getName()
-                + ", menu[0].price=" + request.getMenus().get(0).getPrice();
+        restaurantService.createRestaurant(request);
     }
 
     @PutMapping("/restaurant/{restaurantId}")
-    public String editRestaurant(
+    public void editRestaurant(
             @PathVariable Long restaurantId,
             @RequestBody CreateAndEditRestaurantRequest request
     ) {
-        return "This is editRestaurant, " + restaurantId + ", name=" + request.getName() + ", address=" + request.getAddress();
+        restaurantService.editRestaurant(restaurantId, request);
     }
 
     @DeleteMapping("/restaurant/{restaurantId}")
-    public String deleteRestaurant(
+    public void deleteRestaurant(
             @PathVariable Long restaurantId
     ) {
-        return "This is deleteRestaurant," + restaurantId;
+        restaurantService.deleteRestaurant(restaurantId);
     }
 }
